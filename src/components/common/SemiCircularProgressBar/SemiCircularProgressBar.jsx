e61f6078c5c0c2f60d6a638526243a182172db22
= <=>
2022-08-18 10:36:20 -0500

    refactor semi circular progress bar component

--- src/components/common/SemiCircularProgressBar/SemiCircularProgressBar.jsx
+++ src/components/common/SemiCircularProgressBar/SemiCircularProgressBar.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 
-import './SemiCircularProgressBar.scss'
+import './SemiCircularProgressbar.scss'
 
-const SemiCircularProgressBar = ({ percent = 50 }) => {
+const SemiCircularProgressbar = ({ percent = 50 }) => {
   return (
-    <div className="semi-circular-progress-bar" style={{ '--num': percent }}>
-      <div class="dot"></div>
-      <svg>
-        <defs>
-          <linearGradient id="gradient-color">
-            <stop offset="0%" stop-color="#ff575f" />
-            <stop offset="25%" stop-color="#fa8a07" />
-            <stop offset="50%" stop-color="#ffc542" />
-            <stop offset="75%" stop-color="#b6d534" />
-            <stop offset="100%" stop-color="#93dd2d" />
-          </linearGradient>
-        </defs>
-        <circle class="empty" cx="100" cy="100" r="90" />
-        <circle class="progress-bar" cx="100" cy="100" r="90" />
-      </svg>
-      <div class="content">{percent}%</div>
+    <div className="semi-circular-progressbar-wrapper">
+      <div className="semi-circular-progressbar" style={{ '--percent': percent }}>
+        <div className="dot"></div>
+        <svg>
+          <defs>
+            <linearGradient id="gradient-color">
+              <stop offset="0%" stopColor="#ff575f" />
+              <stop offset="25%" stopColor="#fa8a07" />
+              <stop offset="50%" stopColor="#ffc542" />
+              <stop offset="75%" stopColor="#b6d534" />
+              <stop offset="100%" stopColor="#93dd2d" />
+            </linearGradient>
+          </defs>
+          <circle className="empty" cx="100" cy="100" r="90" />
+          <circle className="progressbar" cx="100" cy="100" r="90" />
+        </svg>
+        <div className="content">{percent}%</div>
+      </div>
     </div>
   )
 }
 
-export default SemiCircularProgressBar
+export default SemiCircularProgressbar
