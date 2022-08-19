3e43521305f697a3676512c3bcc59d8c7951b0ea
= <=>
2022-08-19 16:55:58 -0500

    Add carousel for business units and unit deparments

--- src/views/budget/components/BusinessUnits.jsx
+++ src/views/budget/components/BusinessUnits.jsx
@@ -1,122 +1,72 @@
+import Slider from 'react-slick'
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 import SemiCircularProgressbar from 'components/common/SemiCircularProgressbar/SemiCircularProgressbar'
 
-const BusinessUnits = (props) => {
+import BusinessUnitsData from 'mock/BusinessUnits.json'
+
+const settings = {
+  infinite: false,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1
+}
+
+const BusinessUnits = () => {
   return (
     <Card className="section-gray">
       <Card.Body>
         <h2 className="budget-subtitle">Business Units</h2>
         <Row>
           <Col>
-            <Card className="mb-3">
-              <Card.Body>
-                <Card.Title className="text-center mb-4">Unit 1</Card.Title>
-                <div className="d-flex justify-content-evenly align-items-center">
-                  <SemiCircularProgressbar percent="20" radio="70" circledContent />
-                  <div>
-                    <Card.Subtitle>Spending</Card.Subtitle>
-                    <h2>$10,000</h2>
-                    <Card.Subtitle>Year to date Spend</Card.Subtitle>
-                    <h3>$750,000</h3>
-                  </div>
-                </div>
-
-                <Card className="card-color card-green mt-3">
-                  <Card.Body className="d-flex justify-content-between align-items-center">
-                    <div>
-                      <Card.Subtitle className="fw-bold">Variance in spend</Card.Subtitle>
-                      <h2>$50,000</h2>
+            <Slider {...settings}>
+              {BusinessUnitsData.map((businessUnit) => (
+                <Card className="mb-3" key={businessUnit.title}>
+                  <Card.Body>
+                    <Card.Title className="text-center mb-4">{businessUnit.title}</Card.Title>
+                    <div className="d-flex justify-content-evenly align-items-center">
+                      <SemiCircularProgressbar percent={businessUnit.percent} radio="70" circledContent />
+                      <div>
+                        <Card.Subtitle>Spending</Card.Subtitle>
+                        <h2>{businessUnit.spending}</h2>
+                        <Card.Subtitle>Year to date Spend</Card.Subtitle>
+                        <h3>{businessUnit.year_date_spend}</h3>
+                      </div>
                     </div>
-                    <div className="text-end">
-                      <Card.Subtitle>Available Budget</Card.Subtitle>
-                      <h3>$200,000</h3>
-                      <Card.Subtitle>Planned Spend</Card.Subtitle>
-                      <h3>$10,000</h3>
+
+                    <Card
+                      className={`card-color ${
+                        businessUnit.percent < 34
+                          ? 'card-green'
+                          : businessUnit.percent < 68
+                          ? 'card-orange'
+                          : 'card-red'
+                      } mt-3`}
+                    >
+                      <Card.Body className="d-flex justify-content-between align-items-center">
+                        <div>
+                          <Card.Subtitle className="fw-bold">Variance in spend</Card.Subtitle>
+                          <h2>{businessUnit.variance_in_spend}</h2>
+                        </div>
+                        <div className="text-end">
+                          <Card.Subtitle>Available Budget</Card.Subtitle>
+                          <h3>{businessUnit.available_budget}</h3>
+                          <Card.Subtitle>Planned Spend</Card.Subtitle>
+                          <h3>{businessUnit.planned_spend}</h3>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                    <div className="text-center mt-4 mb-3">
+                      <button className="btn btn-app rounded" type="button">
+                        Details
+                      </button>
                     </div>
                   </Card.Body>
                 </Card>
-                <div className="text-center mt-4 mb-3">
-                  <button className="btn btn-app rounded" type="button">
-                    Details
-                  </button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card className="mb-3">
-              <Card.Body>
-                <Card.Title className="text-center mb-4">Unit 2</Card.Title>
-                <div className="d-flex justify-content-evenly align-items-center">
-                  <SemiCircularProgressbar percent="60" radio="70" circledContent />
-                  <div>
-                    <Card.Subtitle>Spending</Card.Subtitle>
-                    <h2>$500,000</h2>
-                    <Card.Subtitle>Year to date Spend</Card.Subtitle>
-                    <h3>$750,000</h3>
-                  </div>
-                </div>
-
-                <Card className="card-color card-orange mt-3">
-                  <Card.Body className="d-flex justify-content-between align-items-center">
-                    <div>
-                      <Card.Subtitle className="fw-bold">Variance in spend</Card.Subtitle>
-                      <h2>$50,000</h2>
-                    </div>
-                    <div className="text-end">
-                      <Card.Subtitle>Available Budget</Card.Subtitle>
-                      <h3>$200,000</h3>
-                      <Card.Subtitle>Planned Spend</Card.Subtitle>
-                      <h3>$10,000</h3>
-                    </div>
-                  </Card.Body>
-                </Card>
-                <div className="text-center mt-4 mb-3">
-                  <button className="btn btn-app rounded" type="button">
-                    Details
-                  </button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card className="mb-3">
-              <Card.Body>
-                <Card.Title className="text-center mb-4">Unit 3</Card.Title>
-                <div className="d-flex justify-content-evenly align-items-center">
-                  <SemiCircularProgressbar percent="90" radio="70" circledContent />
-                  <div>
-                    <Card.Subtitle>Spending</Card.Subtitle>
-                    <h2>$750,000</h2>
-                    <Card.Subtitle>Year to date Spend</Card.Subtitle>
-                    <h3>$750,000</h3>
-                  </div>
-                </div>
-
-                <Card className="card-color card-red mt-3">
-                  <Card.Body className="d-flex justify-content-between align-items-center">
-                    <div>
-                      <Card.Subtitle className="fw-bold">Variance in spend</Card.Subtitle>
-                      <h2>$50,000</h2>
-                    </div>
-                    <div className="text-end">
-                      <Card.Subtitle>Available Budget</Card.Subtitle>
-                      <h3>$200,000</h3>
-                      <Card.Subtitle>Planned Spend</Card.Subtitle>
-                      <h3>$10,000</h3>
-                    </div>
-                  </Card.Body>
-                </Card>
-                <div className="text-center mt-4 mb-3">
-                  <button className="btn btn-app rounded" type="button">
-                    Details
-                  </button>
-                </div>
-              </Card.Body>
-            </Card>
+              ))}
+            </Slider>
           </Col>
         </Row>
       </Card.Body>
--- src/views/budget/components/UnitDeparments.jsx
+++ src/views/budget/components/UnitDeparments.jsx
@@ -1,8 +1,18 @@
+import Slider from 'react-slick'
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 import CircularProgressbar from 'components/common/CircularProgressbar/CircularProgressbar'
+
+import UnitDeparmentsData from 'mock/UnitDeparments.json'
+
+const settings = {
+  infinite: false,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1
+}
 
 const UnitDeparments = () => {
   return (
@@ -12,148 +22,65 @@ const UnitDeparments = () => {
 
         <Row>
           <Col>
-            <Card>
-              <Card.Body>
-                <div className="unit-deparments-header">
-                  <div>
-                    <Card.Subtitle>Department</Card.Subtitle>
-                    <h5>Lorem Ipsum Dolor</h5>
-                  </div>
-                  <div className="text-end">
-                    <Card.Subtitle>Trend</Card.Subtitle>
-                    <h5>9392939129</h5>
-                  </div>
-                </div>
+            <Slider {...settings}>
+              {UnitDeparmentsData.map((unitDeparment) => {
+                let color = 'red'
+                if (unitDeparment.percent < 34) {
+                  color = 'green'
+                } else if (unitDeparment.percent < 67) {
+                  color = 'orange'
+                }
 
-                <Card className="card-outline-green mb-3">
-                  <Card.Body className="d-flex justify-content-evenly align-items-center">
-                    <CircularProgressbar radio="50" percent="50" color="green" />
-                    <div>
-                      <Card.Subtitle>Spending</Card.Subtitle>
-                      <h2>$500,000</h2>
-                      <Card.Subtitle>Year to date Spend</Card.Subtitle>
-                      <h3>$750,000</h3>
-                    </div>
-                  </Card.Body>
-                </Card>
+                return (
+                  <Card key={unitDeparment.trend}>
+                    <Card.Body>
+                      <div className="unit-deparments-header">
+                        <div>
+                          <Card.Subtitle>Department</Card.Subtitle>
+                          <h5>{unitDeparment.department}</h5>
+                        </div>
+                        <div className="text-end">
+                          <Card.Subtitle>Trend</Card.Subtitle>
+                          <h5>{unitDeparment.trend}</h5>
+                        </div>
+                      </div>
 
-                <Card className="card-outline-green mb-3">
-                  <Card.Body className="d-flex justify-content-between align-items-center">
-                    <div>
-                      <h5 className="text-gray mb-1">Variance in spend</h5>
-                      <h2>$50,000</h2>
-                    </div>
-                    <div className="text-end">
-                      <Card.Subtitle>Available Budget</Card.Subtitle>
-                      <h4>$200,000</h4>
-                      <Card.Subtitle>Planned Spend</Card.Subtitle>
-                      <h4>$10,000</h4>
-                    </div>
-                  </Card.Body>
-                </Card>
-                <div className="text-center mt-4 mb-3">
-                  <button className="btn btn-app rounded" type="button">
-                    Details
-                  </button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <Card.Body>
-                <div className="unit-deparments-header">
-                  <div>
-                    <Card.Subtitle>Department</Card.Subtitle>
-                    <h5>Lorem Ipsum Dolor</h5>
-                  </div>
-                  <div className="text-end">
-                    <Card.Subtitle>Trend</Card.Subtitle>
-                    <h5>9392939129</h5>
-                  </div>
-                </div>
+                      <Card className={`card-outline-${color} mb-3`}>
+                        <Card.Body className="d-flex justify-content-evenly align-items-center">
+                          <CircularProgressbar radio="50" percent={unitDeparment.percent} color={color} />
+                          <div>
+                            <Card.Subtitle>Spending</Card.Subtitle>
+                            <h2>{unitDeparment.spending}</h2>
+                            <Card.Subtitle>Year to date Spend</Card.Subtitle>
+                            <h3>{unitDeparment.year_date_spend}</h3>
+                          </div>
+                        </Card.Body>
+                      </Card>
 
-                <Card className="card-outline-orange mb-3">
-                  <Card.Body className="d-flex justify-content-evenly align-items-center">
-                    <CircularProgressbar radio="50" percent="60" color="orange" />
-                    <div>
-                      <Card.Subtitle>Spending</Card.Subtitle>
-                      <h2>$500,000</h2>
-                      <Card.Subtitle>Year to date Spend</Card.Subtitle>
-                      <h3>$750,000</h3>
-                    </div>
-                  </Card.Body>
-                </Card>
-
-                <Card className="card-outline-orange mb-3">
-                  <Card.Body className="d-flex justify-content-between align-items-center">
-                    <div>
-                      <h5 className="text-gray mb-1">Variance in spend</h5>
-                      <h2>$50,000</h2>
-                    </div>
-                    <div className="text-end">
-                      <Card.Subtitle>Available Budget</Card.Subtitle>
-                      <h4>$200,000</h4>
-                      <Card.Subtitle>Planned Spend</Card.Subtitle>
-                      <h4>$10,000</h4>
-                    </div>
-                  </Card.Body>
-                </Card>
-                <div className="text-center mt-4 mb-3">
-                  <button className="btn btn-app rounded" type="button">
-                    Details
-                  </button>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <Card.Body>
-                <div className="unit-deparments-header">
-                  <div>
-                    <Card.Subtitle>Department</Card.Subtitle>
-                    <h5>Lorem Ipsum Dolor</h5>
-                  </div>
-                  <div className="text-end">
-                    <Card.Subtitle>Trend</Card.Subtitle>
-                    <h5>9392939129</h5>
-                  </div>
-                </div>
-
-                <Card className="card-outline-red mb-3">
-                  <Card.Body className="d-flex justify-content-evenly align-items-center">
-                    <CircularProgressbar radio="50" percent="90" color="red" />
-                    <div>
-                      <Card.Subtitle>Spending</Card.Subtitle>
-                      <h2>$500,000</h2>
-                      <Card.Subtitle>Year to date Spend</Card.Subtitle>
-                      <h3>$750,000</h3>
-                    </div>
-                  </Card.Body>
-                </Card>
-
-                <Card className="card-outline-red mb-3">
-                  <Card.Body className="d-flex justify-content-between align-items-center">
-                    <div>
-                      <h5 className="text-gray mb-1">Variance in spend</h5>
-                      <h2>$50,000</h2>
-                    </div>
-                    <div className="text-end">
-                      <Card.Subtitle>Available Budget</Card.Subtitle>
-                      <h4>$200,000</h4>
-                      <Card.Subtitle>Planned Spend</Card.Subtitle>
-                      <h4>$10,000</h4>
-                    </div>
-                  </Card.Body>
-                </Card>
-                <div className="text-center mt-4 mb-3">
-                  <button className="btn btn-app rounded" type="button">
-                    Details
-                  </button>
-                </div>
-              </Card.Body>
-            </Card>
+                      <Card className={`card-outline-${color} mb-3`}>
+                        <Card.Body className="d-flex justify-content-between align-items-center">
+                          <div>
+                            <h5 className="text-gray mb-1">Variance in spend</h5>
+                            <h2>{unitDeparment.variance_in_spend}</h2>
+                          </div>
+                          <div className="text-end">
+                            <Card.Subtitle>Available Budget</Card.Subtitle>
+                            <h4>{unitDeparment.available_budget}</h4>
+                            <Card.Subtitle>Planned Spend</Card.Subtitle>
+                            <h4>{unitDeparment.planned_spend}</h4>
+                          </div>
+                        </Card.Body>
+                      </Card>
+                      <div className="text-center mt-4 mb-3">
+                        <button className="btn btn-app rounded" type="button">
+                          Details
+                        </button>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                )
+              })}
+            </Slider>
           </Col>
         </Row>
       </Card.Body>
